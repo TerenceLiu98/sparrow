dc2e02a83109b4d42101bdc8abdfeb3d73737063
Terence Liu <terenceliu1012@gmail.com>
2025-02-05 11:58:37 +0000

    fix typo

--- model/modelling_sparrow.py
+++ model/modelling_sparrow.py
@@ -13,14 +13,13 @@ def rotate_half(x):
     x1, x2 = x.chunk(2, dim=-1)
     return torch.cat((-x2, x1), dim=-1)
 
-# 应用位置编码
 def apply_rotate_pos_emb(q, k, cos, sin, unsqueeze_dim=2):
     
-    cos = cos.unsqueeze(unsqueeze_dim) # (1, seq_len, 1, dim)
-    sin = sin.unsqueeze(unsqueeze_dim) # (1, seq_len, 1, dim)
+    cos = cos.unsqueeze(unsqueeze_dim) 
+    sin = sin.unsqueeze(unsqueeze_dim)
    
-    q_embed = (q*cos) + (rotate_half(q)*sin)  # (batch_size, seq_len, head_num, dim) * (1, seq_len, 1, dim) = (batch_size, seq_len, head_num, dim) 广播
-    k_embed = (k*cos) + (rotate_half(k)*sin)  # (batch_size, seq_len, head_num, dim) * (1, seq_len, 1, dim) = = (batch_size, seq_len, head_num, dim) 广播
+    q_embed = (q*cos) + (rotate_half(q)*sin) 
+    k_embed = (k*cos) + (rotate_half(k)*sin) 
     
     return q_embed, k_embed
 
@@ -29,17 +28,17 @@ class RotaryEmbedding(nn.Module):
         super(RotaryEmbedding, self).__init__()
         self.hidden_size = dim
         self.max_seq_len = max_seq_len
-        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim))  # 形状(dim/2)
-        t = torch.arange(max_seq_len).float().unsqueeze(1)  # 形状(max_seq_len, 1)
-        freqs = t @ inv_freq.unsqueeze(0)  #(max_seq_len, 1)*(1, dim/2) = (max_seq_len, dim/2)
-        freqs = torch.cat((freqs, freqs), dim=-1)  # (max_seq_len, dim)
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim))  
+        t = torch.arange(max_seq_len).float().unsqueeze(1)
+        freqs = t @ inv_freq.unsqueeze(0) 
+        freqs = torch.cat((freqs, freqs), dim=-1)
         
         self.register_buffer("cos_cached", freqs.cos())
         self.register_buffer("sin_cached", freqs.sin())
         
     def forward(self, q, k):
-        cos = self.cos_cached[:q.shape[1], :].unsqueeze(0)  # (1, seq_len, dim)
-        sin = self.sin_cached[:q.shape[1], :].unsqueeze(0)  # (1, seq_len, dim)
+        cos = self.cos_cached[:q.shape[1], :].unsqueeze(0) 
+        sin = self.sin_cached[:q.shape[1], :].unsqueeze(0)
         return apply_rotate_pos_emb(q, k, cos, sin)
     
 
